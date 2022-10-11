da99a918ed85f779b98be1dd7d3c0c8ea032e633
hamp13773 <105466669+hamp13773@users.noreply.github.com>
2022-10-11 03:52:13 +0300

    1

--- Enemy.cs
+++ Enemy.cs
@@ -5,10 +5,4 @@ using UnityEngine;
 [RequireComponent(typeof(GameObject))]
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private GameObject _enemy;
-
-    private void OnEnable()
-    {
-        _enemy = GetComponent<GameObject>();
-    }
 }
--- Spawner.cs
+++ Spawner.cs
@@ -3,5 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
-{ 
+{
+    [SerializeField] private Transform[] _spawnPositions;
+    [SerializeField] private Enemy _enemy;
+    [SerializeField] private float _spawnDelay;
+    [SerializeField] private float _objectCount;
+
+    private int _currentIndex;
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        while (_objectCount != 0)
+        {
+            var waitForSeconds = new WaitForSecondsRealtime(_spawnDelay);
+            yield return waitForSeconds;
+            Instantiate(_enemy, _spawnPositions[_currentIndex].transform.position, Quaternion.identity);
+            _currentIndex++;
+            _objectCount--;
+
+            if (_currentIndex >= _spawnPositions.Length)
+            {
+                _currentIndex = 0;
+            }
+        }
+    }
 }
